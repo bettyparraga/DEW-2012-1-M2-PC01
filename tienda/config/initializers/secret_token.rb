376f2686493544573e159da2e77ce333782d2478
Betty Parraga <bparraga@hotmail.com>
2012-09-23 11:22:08 -0500

    Crear el proyecto tienda

---
+++ tienda/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tienda::Application.config.secret_token = 'ca6931b14b2d880d7743aa39d7ac9737dae61408fc61a79b6e11cb3154502073b1ba22d51b091b7230ff417f1575e195879fcd218d985568a1c8dcc497c1727a'
